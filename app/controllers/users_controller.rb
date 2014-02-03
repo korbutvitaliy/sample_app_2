5a4e5f298170e7d0fc488ef57aaa08dca9370673
vitaliy <korbutvitaliy@gmail.com>
2014-02-03 22:59:06 +0300

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,19 @@ before_action :admin_user,     only: :destroy
     end
   end
 
+def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
    private
  	
  	def admin_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp2::Application.routes.draw do
   root  'static_pages#home'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
-  resources :microposts, only: [:create, :destroy]
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+    resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
    match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   
